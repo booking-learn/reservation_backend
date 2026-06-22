6c8f6bc2120163e0855acded8513c2864f0b64af
pbenaja <benaja-phoba.phoba.1@ulaval.ca>
2026-06-22 12:02:33 -0400

    fix: filter chain in security config

--- src/main/java/ca/vetClinic/infra/security/SecurityConfig.java
+++ src/main/java/ca/vetClinic/infra/security/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
 		http.csrf(csrf -> csrf.disable())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(auth -> auth
-						.requestMatchers("/api/auth/**", "/v3/api-docs/**", "/v3/api-docs", "/swagger-ui/**",
+						.requestMatchers("/auth/**", "/v3/api-docs/**", "/v3/api-docs", "/swagger-ui/**",
 								"/actuator/health", "/swagger-ui/index.html", "/swagger-resources/**", "/webjars/**")
 						.permitAll().anyRequest().authenticated())
 				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
